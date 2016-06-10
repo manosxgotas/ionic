14a4cf7975c0fde3824fe6dd295905dddb127661
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-06-10 00:44:32 -0300

    Implementación de estilos y restricciones para validación de inputs y cambio de datapicker por uib-datepicker en la fecha de nacimiento

--- www/js/controllers/registro.js
+++ www/js/controllers/registro.js
@@ -13,20 +13,23 @@ angular.module('donacion')
       $scope.localidades = DireccionesService.getLocalidades(idprov).query();
     };
 
-    console.log($scope.provinciaId);
+    // <---- DatePicker fecha de nacimiento
+    $scope.nacimientoDPOptions = {
+      initDate: new Date(1950, 0, 1),
+      minDate: new Date(1900, 0, 1),
+      maxDate: new Date(2010, 0, 1)
+    };
 
-    $('#nacimiento').datepicker({
-      language: 'es',
-      defaultViewDate: {
-        year: 1900,
-        month: 01,
-        day: 01
-      },
-      startDate: '01/01/1900',
-      endDate: '31/12/2010',
-      startView: 'decade',
-      format: 'dd/mm/yyyy'
-    });
+    $scope.nacimientoDP = {
+      opened: false
+    };
+
+    $scope.format = "dd/MM/yyyy";
+
+    $scope.openDatePicker = function() {
+      $scope.nacimientoDP.opened = true;
+    };
+    // DatePicker fecha de nacimiento ---->
 
     $scope.signup = function() {
       RegistroService.registrarse($scope.datosDonante);

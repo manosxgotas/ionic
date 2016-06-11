0abfe594f66a31035db93b700050b0d2a1170de5
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-06-10 21:22:59 -0300

    Implementación de lógica para los campos de confirmación de correo y contraseña, y documentación del controlador de registro

--- www/js/controllers/registro.js
+++ www/js/controllers/registro.js
@@ -1,17 +1,51 @@
 angular.module('donacion')
   .controller('RegistroController', function ($http, $scope, RegistroService, DireccionesService) {
 
+    // Inicializo variables
+
+    // Booleano para comprobar si los inputs de confirmación de pass y email son correctos.
+    $scope.correoCheck = false;
+    $scope.passwordCheck = false;
+
+    // Objeto que contendrá la información del donante a enviar a la API.
     $scope.datosDonante = {
       usuario: undefined,
       donante: undefined,
-      direccion: undefined,
+      direccion: undefined
     };
 
+
+    // Obtengo las provincias de la API.
     $scope.provincias = DireccionesService.getProvincias().query();
 
+
+    // Según la provincia elegida obtengo sus localidades.
     $scope.obtenerLocalidades = function(idprov) {
       $scope.localidades = DireccionesService.getLocalidades(idprov).query();
     };
+
+
+    // Métodos para comprobar si los inputs de confirmación de pass y email son correctos y seteo validez.
+    $scope.confirmarCorreo = function (formRegistro, confirmCorreo) {
+      if ($scope.datosDonante.usuario.email == confirmCorreo) {
+        $scope.correoCheck = true;
+        formRegistro.confirmCorreo.$setValidity('checkTrue', true);
+      } else {
+        $scope.correoCheck = false;
+        formRegistro.confirmCorreo.$setValidity('checkTrue', false);
+      }
+    };
+
+    $scope.confirmarContrasenia = function (formRegistro, confirmContrasenia) {
+      if ($scope.datosDonante.usuario.password == confirmContrasenia) {
+        $scope.passwordCheck = true;
+        formRegistro.confirmContrasenia.$setValidity('checkTrue', true);
+      } else {
+        $scope.passwordCheck = false;
+        formRegistro.confirmContrasenia.$setValidity('checkTrue', false);
+      }
+    };
+
 
     // <---- DatePicker fecha de nacimiento
     $scope.nacimientoDPOptions = {
@@ -30,6 +64,7 @@ angular.module('donacion')
       $scope.nacimientoDP.opened = true;
     };
     // DatePicker fecha de nacimiento ---->
+
 
     $scope.signup = function() {
       RegistroService.registrarse($scope.datosDonante);

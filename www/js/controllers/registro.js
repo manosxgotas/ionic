888e6276d1a5db62d52f754be01825e67fcc0afa
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-06-24 19:25:34 -0300

    Cambio de plantilla, controlador y servicio de registro para obtener nuevos atributos del donante desde la API, añadir campos y realizar el POST

--- www/js/controllers/registro.js
+++ www/js/controllers/registro.js
@@ -1,72 +1,89 @@
 angular.module('donacion')
-  .controller('RegistroController', function ($http, $scope, RegistroService, DireccionesService) {
+  .controller('RegistroController',
+    function ($http,
+              $scope,
+              RegistroService,
+              DireccionesService,
+              TiposDocumentosService,
+              NacionalidadesService,
+              GruposSanguineosService
+              )
+    {
 
-    // Inicializo variables
+      // Inicializo variables
 
-    // Booleano para comprobar si los inputs de confirmación de pass y email son correctos.
-    $scope.correoCheck = false;
-    $scope.passwordCheck = false;
+      // Booleano para comprobar si los inputs de confirmación de pass y email son correctos.
+      $scope.correoCheck = false;
+      $scope.passwordCheck = false;
 
-    // Objeto que contendrá la información del donante a enviar a la API.
-    $scope.datosDonante = {
-      usuario: undefined,
-      donante: undefined,
-      direccion: undefined
-    };
-
-
-    // Obtengo las provincias de la API.
-    $scope.provincias = DireccionesService.getProvincias().query();
+      // Objeto que contendrá la información del donante a enviar a la API.
+      $scope.datosDonante = {
+        usuario: undefined,
+        donante: undefined,
+        direccion: undefined
+      };
 
 
-    // Según la provincia elegida obtengo sus localidades.
-    $scope.obtenerLocalidades = function(idprov) {
-      $scope.localidades = DireccionesService.getLocalidades(idprov).query();
-    };
+      // Obtengo las provincias de la API.
+      $scope.provincias = DireccionesService.getProvincias().query();
+
+      // Obtengo los tipos de documentos de la API
+      $scope.tiposDocumentos = TiposDocumentosService.query();
+
+      // Obtengo las nacionalidades de la API
+      $scope.nacionalidades = NacionalidadesService.query();
+
+      // Obtengo los grupos sanguíneos de la API
+      $scope.gruposSanguineos = GruposSanguineosService.query();
+
+      // Según la provincia elegida obtengo sus localidades.
+      $scope.obtenerLocalidades = function(idprov) {
+        $scope.localidades = DireccionesService.getLocalidades(idprov).query();
+      };
 
 
-    // Métodos para comprobar si los inputs de confirmación de pass y email son correctos y seteo validez.
-    $scope.confirmarCorreo = function (formRegistro, confirmCorreo) {
-      if ($scope.datosDonante.usuario.email == confirmCorreo) {
-        $scope.correoCheck = true;
-        formRegistro.confirmCorreo.$setValidity('checkTrue', true);
-      } else {
-        $scope.correoCheck = false;
-        formRegistro.confirmCorreo.$setValidity('checkTrue', false);
+      // Métodos para comprobar si los inputs de confirmación de pass y email son correctos y seteo validez.
+      $scope.confirmarCorreo = function (formRegistro, confirmCorreo) {
+        if ($scope.datosDonante.usuario.email == confirmCorreo) {
+          $scope.correoCheck = true;
+          formRegistro.confirmCorreo.$setValidity('checkTrue', true);
+        } else {
+          $scope.correoCheck = false;
+          formRegistro.confirmCorreo.$setValidity('checkTrue', false);
+        }
+      };
+
+      $scope.confirmarContrasenia = function (formRegistro, confirmContrasenia) {
+        if ($scope.datosDonante.usuario.password == confirmContrasenia) {
+          $scope.passwordCheck = true;
+          formRegistro.confirmContrasenia.$setValidity('checkTrue', true);
+        } else {
+          $scope.passwordCheck = false;
+          formRegistro.confirmContrasenia.$setValidity('checkTrue', false);
+        }
+      };
+
+
+      // <---- DatePicker fecha de nacimiento
+      $scope.nacimientoDPOptions = {
+        initDate: new Date(1950, 0, 1),
+        minDate: new Date(1900, 0, 1),
+        maxDate: new Date(2010, 0, 1)
+      };
+
+      $scope.nacimientoDP = {
+        opened: false
+      };
+
+      $scope.format = "dd/MM/yyyy";
+
+      $scope.openDatePicker = function() {
+        $scope.nacimientoDP.opened = true;
+      };
+      // DatePicker fecha de nacimiento ---->
+
+
+      $scope.signup = function() {
+        RegistroService.registrarse($scope.datosDonante);
       }
-    };
-
-    $scope.confirmarContrasenia = function (formRegistro, confirmContrasenia) {
-      if ($scope.datosDonante.usuario.password == confirmContrasenia) {
-        $scope.passwordCheck = true;
-        formRegistro.confirmContrasenia.$setValidity('checkTrue', true);
-      } else {
-        $scope.passwordCheck = false;
-        formRegistro.confirmContrasenia.$setValidity('checkTrue', false);
-      }
-    };
-
-
-    // <---- DatePicker fecha de nacimiento
-    $scope.nacimientoDPOptions = {
-      initDate: new Date(1950, 0, 1),
-      minDate: new Date(1900, 0, 1),
-      maxDate: new Date(2010, 0, 1)
-    };
-
-    $scope.nacimientoDP = {
-      opened: false
-    };
-
-    $scope.format = "dd/MM/yyyy";
-
-    $scope.openDatePicker = function() {
-      $scope.nacimientoDP.opened = true;
-    };
-    // DatePicker fecha de nacimiento ---->
-
-
-    $scope.signup = function() {
-      RegistroService.registrarse($scope.datosDonante);
-    }
   });

5c4bf9ded112417ed1e1010ce09029d0c6188b87
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-07-01 14:01:30 -0300

    Recepción de nuevos atributos en el controlador del perfil del donante para pasarlos a la vista.

--- www/js/controllers/profile.js
+++ www/js/controllers/profile.js
@@ -6,14 +6,27 @@ angular.module('donacion')
       $scope.perfil = {};
 
       var profile = ProfileService.getProfile().get({}, function () {
-        console.log(profile);
         $scope.perfil.nombre = profile.usuario.first_name;
         $scope.perfil.apellido = profile.usuario.last_name;
         $scope.perfil.username = profile.usuario.username;
         $scope.perfil.foto = profile.foto;
         $scope.perfil.nacimiento = profile.nacimiento;
-        $scope.perfil.gs = profile.grupoSanguineo.nombre;
+
+        switch (profile.genero) {
+          case '1':
+            $scope.perfil.sexo = 'Hombre';
+            break;
+          case '2':
+            $scope.perfil.sexo = 'Mujer';
+            break;
+        }
+
+        if (profile.grupoSanguineo != null) {
+            $scope.perfil.gs = profile.grupoSanguineo.nombre;
+          }
         $scope.perfil.correo = profile.usuario.email;
+        $scope.perfil.peso = profile.peso;
+        $scope.perfil.altura = profile.altura;
       })
     })
 

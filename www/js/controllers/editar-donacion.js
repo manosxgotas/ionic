9b241a4371c233d926428a72731fb336388f9766
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-07-12 20:34:57 -0300

    Creación de controlador y template para la edición de la donación

---
+++ www/js/controllers/editar-donacion.js
@@ -0,0 +1,27 @@
+angular.module('donacion')
+  .controller('EditarDonacionController', function ($http, $scope, $stateParams, AuthService, ProfileService, DonacionesService) {
+
+    $scope.DPOptions = {
+      maxDate: new Date()
+    };
+
+    AuthService.isLogged(function () {
+      $scope.donacion = {};
+
+      $scope.perfil = ProfileService.getProfile().get();
+
+      var data = DonacionesService.infoDonacion().query({id: $stateParams.donacionID}, function () {
+        $scope.donacion = data;
+        $scope.donacion.fecha = moment(data.fechaHora, 'DD/MM/YYYY HH:mm').format('DD/MM/YYYY');
+        $scope.donacion.hora = moment(data.fechaHora, 'DD/MM/YYYY HH:mm').format('HH:mm');
+      });
+
+      $scope.updateDonacion = function(foto) {
+        if (foto != undefined) {
+          DonacionesService.editarDonacion($scope.donacion, foto.file);
+        } else {
+          DonacionesService.editarDonacion($scope.donacion);
+        }
+      }
+    });
+  });

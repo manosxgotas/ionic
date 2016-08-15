bbb47052228955ce60000135c69b0314785ac4ef
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-08-15 16:58:30 -0300

    Modificación de servicio de logoff para recargar la página en lugar de redirigir en caso de que el usuario se encuentre en la sección home.

--- www/js/services/resources/logoff.js
+++ www/js/services/resources/logoff.js
@@ -4,7 +4,13 @@ angular.module('donacion')
 
     function logoff() {
       localStorageService.clearAll();
-      $state.transitionTo('home.inicio');
+
+      // Verifico si estoy o no en la sección home
+      if ($state.current.name.indexOf('home') >= 0) {
+        $state.reload();
+      } else {
+        $state.transitionTo('home.inicio');
+      }
 
       // Algoritmo para eliminar los atributos del rootScope creados "artificialmente".
       for (var prop in $rootScope) {

23988db40329063608026824c004bfbb7f0051e5
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-09-19 01:35:41 -0300

    Sustitución de pantalla de registro por modal y cambio de campos necesarios para registrar a un nuevo donante.

--- www/js/controllers/registro.js
+++ www/js/controllers/registro.js
@@ -6,45 +6,24 @@ angular.module('donacion')
               DireccionesService,
               TiposDocumentosService,
               NacionalidadesService,
-              GruposSanguineosService
+              GruposSanguineosService,
+              $uibModalInstance
               )
     {
 
+      $scope.close = function () {
+        $uibModalInstance.dismiss('cerrar');
+      };
+
       // Inicializo variables
+      $scope.donante = undefined;
 
       // Booleano para comprobar si los inputs de confirmación de pass y email son correctos.
       $scope.correoCheck = false;
-      $scope.passwordCheck = false;
-
-      // Objeto que contendrá la información del donante a enviar a la API.
-      $scope.datosDonante = {
-        usuario: undefined,
-        donante: undefined,
-        direccion: undefined
-      };
-
-
-      // Obtengo las provincias de la API.
-      $scope.provincias = DireccionesService.getProvincias().query();
-
-      // Obtengo los tipos de documentos de la API
-      $scope.tiposDocumentos = TiposDocumentosService.query();
-
-      // Obtengo las nacionalidades de la API
-      $scope.nacionalidades = NacionalidadesService.query();
-
-      // Obtengo los grupos sanguíneos de la API
-      $scope.gruposSanguineos = GruposSanguineosService.query();
-
-      // Según la provincia elegida obtengo sus localidades.
-      $scope.obtenerLocalidades = function(idprov) {
-        $scope.localidades = DireccionesService.getLocalidades(idprov).query();
-      };
-
 
       // Métodos para comprobar si los inputs de confirmación de pass y email son correctos y seteo validez.
       $scope.confirmarCorreo = function (formRegistro, confirmCorreo) {
-        if ($scope.datosDonante.usuario.email == confirmCorreo) {
+        if ($scope.donante.email == confirmCorreo) {
           $scope.correoCheck = true;
           formRegistro.confirmCorreo.$setValidity('checkTrue', true);
         } else {
@@ -53,40 +32,7 @@ angular.module('donacion')
         }
       };
 
-      $scope.confirmarContrasenia = function (formRegistro, confirmContrasenia) {
-        if ($scope.datosDonante.usuario.password == confirmContrasenia) {
-          $scope.passwordCheck = true;
-          formRegistro.confirmContrasenia.$setValidity('checkTrue', true);
-        } else {
-          $scope.passwordCheck = false;
-          formRegistro.confirmContrasenia.$setValidity('checkTrue', false);
-        }
-      };
-
-
-      // <---- DatePicker fecha de nacimiento
-      $scope.nacimientoDPOptions = {
-        initDate: new Date(1950, 0, 1),
-        minDate: new Date(1900, 0, 1),
-        maxDate: new Date(2010, 0, 1),
-        datepickerMode: 'year'
-      };
-
-      $scope.nacimientoDP = {
-        opened: false
-      };
-
-      $scope.format = "dd/MM/yyyy";
-
-      $scope.openDatePicker = function() {
-        $scope.nacimientoDP.opened = true;
-      };
-
-
-      // DatePicker fecha de nacimiento ---->
-
-
       $scope.signup = function() {
-        RegistroService.registrarse($scope.datosDonante);
+        RegistroService.registrarse($scope.donante);
       }
   });

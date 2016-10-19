337490ba98b1a4979835f6031ed4da3a4924c3ef
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-10-19 17:15:16 -0300

    Integración de sentimientos en modal visualizar donación

--- www/js/controllers/visualizar-donacion.js
+++ www/js/controllers/visualizar-donacion.js
@@ -3,6 +3,47 @@ angular.module('donacion')
 
     $scope.donacion = DonacionesService.infoDonacion().query({id: idDonacion}, function () {
       $scope.donacion.fecha = moment($scope.donacion.fechaHora).format('')
+
+      if($scope.donacion.estado) {
+        switch ($scope.donacion.estado) {
+          case "1":
+            $scope.sentimiento = {
+              img: "../img/sentimientos/muy-mal.png",
+              nombre: "muy mal"
+            };
+            break;
+          case "2":
+            $scope.sentimiento = {
+              img: "../img/sentimientos/mal.png",
+              nombre: "mal"
+            };
+            break;
+          case "3":
+            $scope.sentimiento = {
+              img: "../img/sentimientos/descompuesto.png",
+              nombre: "descompuesto"
+            };
+            break;
+          case "4":
+            $scope.sentimiento = {
+              img: "../img/sentimientos/bien.png",
+              nombre: "bien"
+            };
+            break;
+          case "5":
+            $scope.sentimiento = {
+              img: "../img/sentimientos/muy-bien.png",
+              nombre: "muy bien"
+            };
+            break;
+          case "6":
+            $scope.sentimiento = {
+              img: "../img/sentimientos/excelente.png",
+              nombre: "excelente"
+            };
+            break;
+        }
+      }
     });
 
     $scope.cerrarModal = function () {

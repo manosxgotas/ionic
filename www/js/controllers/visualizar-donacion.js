67646539c54f66fb2a0306063f1879f545c1adef
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-10-19 12:45:05 -0300

    Eliminación de captura de eventos de loading-bar en modal de visualización de donación

--- www/js/controllers/visualizar-donacion.js
+++ www/js/controllers/visualizar-donacion.js
@@ -1,20 +1,9 @@
 angular.module('donacion')
   .controller('VisualizarDonacionController', function ($http, $scope, $uibModalInstance, DonacionesService, idDonacion) {
 
-    $scope.loading = false;
-
-    $scope.$on('cfpLoadingBar:loading', function () {
-      $scope.loading = true;
-    });
-
-    $scope.$on('cfpLoadingBar:completed', function () {
-      $scope.loading = false;
-    });
-
     $scope.donacion = DonacionesService.infoDonacion().query({id: idDonacion}, function () {
       $scope.donacion.fecha = moment($scope.donacion.fechaHora).format('')
     });
-    console.log($scope.donacion);
 
     $scope.cerrarModal = function () {
       $uibModalInstance.dismiss();
